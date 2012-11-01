842633fc1678fde798eae814099525143e7b4fd5
Scott Corley <scorley@ehirelabs.com>
2012-11-01 10:53:28 -0400

    Adding comments for proxy setup

--- Resources/shared/Lib.js
+++ Resources/shared/Lib.js
@@ -1,3 +1,21 @@
+/*
+Used NGINX to host the web with additional proxies configured at the address supplied below at 
+lines 19 and 20.
+
+	location /shows {
+       proxy_pass      http://api.sonicliving.com/api/?&q=pop&key=SONIC_LIVING_API_KEY&radius=20&$args;
+    }
+
+    location /bands {
+       proxy_pass     http://api.emusic.com/artist/info?apiKey=EMUSIC_API_KEY&format=JSON&$args;
+    }
+
+    location /songs {
+       proxy_pass     http://api.emusic.com/track/search?apiKey=EMUSIC_API_KEY&format=JSON&$args;
+    }
+
+*/
+
 var showHost = "PROXY_HOST_ADDRESS";
 var artistSongHost = "PROXY_HOST_ADDRESS";
 	
